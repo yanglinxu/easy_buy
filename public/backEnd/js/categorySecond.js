0b0ddd3ad8f72ac2aee884a8dfa20d8fbb6bb1d0
yanglinxu <28937402@qq.com>
2017-11-12 14:26:51 +0800

    a little

--- public/backEnd/js/categorySecond.js
+++ public/backEnd/js/categorySecond.js
@@ -16,4 +16,25 @@ $(function(){
 	}
 
 	secondDatas()
+
+
+	//点击模态框下拉按钮动态获取数据
+	var dropdown = function(){
+		$('#second_modal').on('click', '#dLabel' ,function(){
+			//发送ajax请求
+			$.ajax({
+				url: '/category/queryTopCategoryPaging',
+				type: 'get',
+				data: {
+					page: 1,
+					pageSize: 20
+				},
+				success: function(res){
+					console.log(res)
+				}
+			})
+		})
+	}
+
+	dropdown()
 })
